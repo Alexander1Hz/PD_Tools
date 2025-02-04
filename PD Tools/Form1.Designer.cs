5cebdc9ec4f1d8fed8375d0aa597077fbcc63d8f
Alexander1Hz <280047530@qq.com>
2025-02-04 21:51:10 +0800

    20250204 upload

--- PD Tools/Form1.Designer.cs
+++ PD Tools/Form1.Designer.cs
@@ -21,32 +21,128 @@
             this.txtWidth = new System.Windows.Forms.TextBox();
             this.txtHeight = new System.Windows.Forms.TextBox();
             this.btnGenerate = new System.Windows.Forms.Button();
-            // 在这里初始化和布局控件
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.评估ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.电压设定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.时序设定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.投产ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cG旋转度ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.设计验证ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.奇偶列图片ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.奇偶行图片ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.版本号ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtWidth
             // 
-            this.txtWidth.Location = new System.Drawing.Point(12, 12);
+            this.txtWidth.Location = new System.Drawing.Point(12, 65);
             this.txtWidth.Name = "txtWidth";
-            this.txtWidth.Size = new System.Drawing.Size(100, 20);
-            this.txtWidth.TabIndex = 0;
+            this.txtWidth.Size = new System.Drawing.Size(100, 21);
+            this.txtWidth.TabIndex = 1;
             // 
             // txtHeight
             // 
-            this.txtHeight.Location = new System.Drawing.Point(12, 38);
+            this.txtHeight.Location = new System.Drawing.Point(12, 92);
             this.txtHeight.Name = "txtHeight";
-            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.Size = new System.Drawing.Size(100, 21);
             this.txtHeight.TabIndex = 1;
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Location = new System.Drawing.Point(12, 64);
+            this.btnGenerate.Location = new System.Drawing.Point(12, 119);
             this.btnGenerate.Name = "btnGenerate";
             this.btnGenerate.Size = new System.Drawing.Size(100, 23);
             this.btnGenerate.TabIndex = 2;
-            this.btnGenerate.Text = "Generate Image";
+            this.btnGenerate.Text = "确认生成";
             this.btnGenerate.UseVisualStyleBackColor = true;
-            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            this.btnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.评估ToolStripMenuItem,
+            this.投产ToolStripMenuItem,
+            this.设计验证ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 25);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 评估ToolStripMenuItem
+            // 
+            this.评估ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.电压设定ToolStripMenuItem,
+            this.时序设定ToolStripMenuItem});
+            this.评估ToolStripMenuItem.Name = "评估ToolStripMenuItem";
+            this.评估ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.评估ToolStripMenuItem.Text = "设计";
+            // 
+            // 电压设定ToolStripMenuItem
+            // 
+            this.电压设定ToolStripMenuItem.Name = "电压设定ToolStripMenuItem";
+            this.电压设定ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.电压设定ToolStripMenuItem.Text = "电压设定";
+            this.电压设定ToolStripMenuItem.Click += new System.EventHandler(this.电压设定ToolStripMenuItem_Click);
+            // 
+            // 时序设定ToolStripMenuItem
+            // 
+            this.时序设定ToolStripMenuItem.Name = "时序设定ToolStripMenuItem";
+            this.时序设定ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.时序设定ToolStripMenuItem.Text = "时序设定";
+            // 
+            // 投产ToolStripMenuItem
+            // 
+            this.投产ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cG旋转度ToolStripMenuItem});
+            this.投产ToolStripMenuItem.Name = "投产ToolStripMenuItem";
+            this.投产ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.投产ToolStripMenuItem.Text = "投产";
+            // 
+            // cG旋转度ToolStripMenuItem
+            // 
+            this.cG旋转度ToolStripMenuItem.Name = "cG旋转度ToolStripMenuItem";
+            this.cG旋转度ToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.cG旋转度ToolStripMenuItem.Text = "CG旋转度";
+            // 
+            // 设计验证ToolStripMenuItem
+            // 
+            this.设计验证ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.奇偶列图片ToolStripMenuItem,
+            this.奇偶行图片ToolStripMenuItem});
+            this.设计验证ToolStripMenuItem.Name = "设计验证ToolStripMenuItem";
+            this.设计验证ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.设计验证ToolStripMenuItem.Text = "设计验证";
+            // 
+            // 奇偶列图片ToolStripMenuItem
+            // 
+            this.奇偶列图片ToolStripMenuItem.Name = "奇偶列图片ToolStripMenuItem";
+            this.奇偶列图片ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.奇偶列图片ToolStripMenuItem.Text = "奇偶列图片";
+            // 
+            // 奇偶行图片ToolStripMenuItem
+            // 
+            this.奇偶行图片ToolStripMenuItem.Name = "奇偶行图片ToolStripMenuItem";
+            this.奇偶行图片ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.奇偶行图片ToolStripMenuItem.Text = "奇偶行图片";
+
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.版本号ToolStripMenuItem});
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(12, 21);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // 版本号ToolStripMenuItem
+            // 
+            this.版本号ToolStripMenuItem.Name = "版本号ToolStripMenuItem";
+            this.版本号ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.版本号ToolStripMenuItem.Text = "版本号";
             // 
             // MainForm
             // 
@@ -54,10 +150,15 @@
             this.Controls.Add(this.btnGenerate);
             this.Controls.Add(this.txtHeight);
             this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
-            this.Text = "Image Generator";
+            this.Text = "PD Tools";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
@@ -65,5 +166,16 @@
         private System.Windows.Forms.TextBox txtWidth;
         private System.Windows.Forms.TextBox txtHeight;
         private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 评估ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 电压设定ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 时序设定ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 投产ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cG旋转度ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 设计验证ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 奇偶列图片ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 奇偶行图片ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 版本号ToolStripMenuItem;
     }
 }
--- PD Tools/Form1.cs
+++ PD Tools/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ImageGeneratorWithUI
 {
@@ -11,37 +12,50 @@ namespace ImageGeneratorWithUI
             InitializeComponent();
         }
 
-        private void btnGenerate_Click(object sender, EventArgs e)
+        private void BtnGenerate_Click(object sender, EventArgs e)
         {
             int width, height;
 
             if (int.TryParse(txtWidth.Text, out width) && int.TryParse(txtHeight.Text, out height))
             {
-                // 使用 SaveFileDialog 让用户选择保存路径和文件名
-                SaveFileDialog saveFileDialog = new SaveFileDialog
+                // 使用 FolderBrowserDialog 让用户选择保存目录
+                FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog
                 {
-                    Filter = "Bitmap files (*.bmp)|*.bmp",
-                    DefaultExt = "bmp",
-                    AddExtension = true
+                    Description = "选择保存图像的目录"
                 };
 
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
                 {
-                    // 获取用户选择的路径
-                    string filePath = saveFileDialog.FileName;
+                    // 获取用户选择的目录
+                    string saveDirectory = folderBrowserDialog.SelectedPath;
+
+                    // 设定图片命名逻辑
+                    string name1 = $"列_奇白偶黑_{width}x{height}";
+                    string name2 = $"列_奇黑偶白_{width}x{height}";
+                    string name3 = $"列_奇红偶黑_{width}x{height}";
+                    string name4 = $"列_奇黑偶红_{width}x{height}";
+                    string name5 = $"列_奇绿偶黑_{width}x{height}";
+                    string name6 = $"列_奇黑偶绿_{width}x{height}";
+                    string name7 = $"列_奇蓝偶黑_{width}x{height}";
+                    string name8 = $"列_奇黑偶蓝_{width}x{height}";
+
 
                     // 调用图像生成逻辑
-                    GenerateImage(width, height, "WhiteOddColumn", Color.White, Color.Black, filePath);
-                    GenerateImage(width, height, "BlackOddColumn", Color.Black, Color.White, filePath.Replace(".bmp","_BlackOddColumn.bmp"));
-                    GenerateImage(width, height, "RedOddColumn", Color.Red, Color.Black, filePath.Replace(".bmp", "_RedOddColumn.bmp"));
-                    GenerateImage(width, height, "BlueOddColumn", Color.Blue, Color.Black, filePath.Replace(".bmp", "_BlueOddColumn.bmp"));
+                    GenerateImage(width, height, name1, Color.White, Color.Black, Path.Combine(saveDirectory, $"{name1}.bmp"));
+                    GenerateImage(width, height, name2, Color.Black, Color.White, Path.Combine(saveDirectory, $"{name2}.bmp"));
+                    GenerateImage(width, height, name3, Color.Red,   Color.Black, Path.Combine(saveDirectory, $"{name3}.bmp"));
+                    GenerateImage(width, height, name4, Color.Black, Color.Red,   Path.Combine(saveDirectory, $"{name4}.bmp"));
+                    GenerateImage(width, height, name5, Color.Green, Color.Black, Path.Combine(saveDirectory, $"{name5}.bmp"));
+                    GenerateImage(width, height, name6, Color.Black, Color.Green, Path.Combine(saveDirectory, $"{name6}.bmp"));
+                    GenerateImage(width, height, name7, Color.Blue,  Color.Black, Path.Combine(saveDirectory, $"{name7}.bmp"));
+                    GenerateImage(width, height, name8, Color.Black, Color.Blue,  Path.Combine(saveDirectory, $"{name8}.bmp"));
 
-                    MessageBox.Show("Images generated successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("图片生成成功！", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
             else
             {
-                MessageBox.Show("Please enter valid width and height.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("请输入有效的分辨率！", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -55,11 +69,11 @@ namespace ImageGeneratorWithUI
                 {
                     if (x % 2 == 0)
                     {
-                        bitmap.SetPixel(x, y, evenColor);
+                        bitmap.SetPixel(x, y, oddColor);
                     }
                     else
                     {
-                        bitmap.SetPixel(x, y, oddColor);
+                        bitmap.SetPixel(x, y, evenColor);
                     }
                 }
             }
@@ -67,8 +81,11 @@ namespace ImageGeneratorWithUI
             bitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp);
         }
 
-       
+        private void 电压设定ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
 
-         
+        }
+
+       
     }
 }
